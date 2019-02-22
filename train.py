6a0274d7e293166281c937597238a6b101660f53
jymsuper <dudans@kaist.ac.kr>
2019-02-22 14:27:12 +0900

    v2

--- train.py
+++ train.py
@@ -12,33 +12,29 @@ import configure as c
 from DB_reader import read_DB_structure
 from model.model import DNN
 
-from utils import get_global_mean_std
-from VAD_Dataset import *
+from utils import get_global_mean_std, read_MRCG
+from VAD_Dataset import VAD_Dataset, VAD_Compose, TruncatedInputfromMFB, ToTensorInput
+import matplotlib.pyplot as plt
 
-
-def load_dataset():
+def load_dataset(val_ratio):
     # Load training set and validation set
-    
-    # Percentage of validation set
-    val_ratio = 10 
     
     # Split training set into training set and validation set according to "val_ratio"
     train_DB, valid_DB = split_train_dev(c.TRAIN_FEAT_DIR, val_ratio) 
-    
-    file_loader = read_MFB # numpy array:(n_frames, n_dims)
+
+    # Calculate global mean and standard deviation for normalization 
+    train_mean, train_std = get_global_mean_std(train_DB)
+
+    file_loader = read_MRCG # numpy array:(n_frames, n_dims)
      
-    transform = transforms.Compose([
+    transform = VAD_Compose([
         TruncatedInputfromMFB(), # numpy array:(1, n_frames, n_dims)
-        ToTensorInput() # torch tensor:(1, n_dims, n_frames)
+        ToTensorInput() # torch tensor:(n_dims*n_frames)
     ])
-    transform_T = ToTensorDevInput()
-   
+    #transform_T = ToTensorDevInput()
     
-    speaker_list = sorted(set(train_DB['speaker_id'])) # len(speaker_list) == n_speakers
-    spk_to_idx = {spk: i for i, spk in enumerate(speaker_list)}
-    
-    train_dataset = DvectorDataset(DB=train_DB, loader=file_loader, transform=transform, spk_to_idx=spk_to_idx)
-    valid_dataset = DvectorDataset(DB=valid_DB, loader=file_loader, transform=transform_T, spk_to_idx=spk_to_idx)
+    train_dataset = VAD_Dataset(DB=train_DB, loader=file_loader, transform=transform)
+    valid_dataset = VAD_Dataset(DB=valid_DB, loader=file_loader, transform=transform)
     
     return train_dataset, valid_dataset
 
@@ -60,6 +56,181 @@ def split_train_dev(train_feat_dir, valid_ratio):
     
     return train_DB, valid_DB
 
+def main():
+    
+    # Set hyperparameters
+    use_cuda = True # use gpu or cpu
+    val_ratio = 10 # Percentage of validation set
+    hidden_size = 512
+    start = 1 # Start epoch
+    n_epochs = 15 # How many epochs?
+    end = start + n_epochs # Last epoch
+
+    lr = 1e-1 # Initial learning rate
+    wd = 1e-4 # Weight decay (L2 penalty)
+    optimizer_type = 'sgd' # ex) sgd, adam, adagrad
+
+    batch_size = 128 # Batch size for training 
+    valid_batch_size = 32 # Batch size for validation
+    use_shuffle = True # Shuffle for training or not
+
+    # Load dataset
+    train_dataset, valid_dataset = load_dataset(val_ratio)
+    
+    log_dir = 'model_saved' # where to save checkpoints
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    
+    input_size = c.FILTER_BANK * c.NUM_FRAMES
+    print('\nInput dimension for training:\n{}\n'.format(input_size))
+    
+    # instantiate model and initialize weights
+    model = DNN(input_size=input_size, hidden_size=hidden_size, num_classes=2)
+    
+    if use_cuda:
+        model.cuda()
+    
+    # define loss function (criterion), optimizer and scheduler
+    criterion = nn.CrossEntropyLoss()
+    optimizer = create_optimizer(optimizer_type, model, lr, wd)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=1, min_lr=1e-4, verbose=1)
+        
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                       batch_size=batch_size,
+                                                       shuffle=use_shuffle)
+    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
+                                                       batch_size=valid_batch_size,
+                                                       shuffle=False)
+                               
+    # to track the average training loss per epoch as the model trains
+    avg_train_losses = []
+    # to track the average validation loss per epoch as the model trains
+    avg_valid_losses = []
+    
+    for epoch in range(start, end):
+        
+        # train for one epoch
+        train_loss = train(train_loader, model, criterion, optimizer, use_cuda, epoch)
+        
+        # evaluate on validation set
+        valid_loss = validate(valid_loader, model, criterion, use_cuda, epoch)
+        
+        scheduler.step(valid_loss, epoch)
+        
+        # calculate average loss over an epoch
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
+        
+        # do checkpointing
+        torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict(),
+                    'optimizer': optimizer.state_dict()},
+                    '{}/checkpoint_{}.pth'.format(log_dir, epoch))
+                    
+    # find position of lowest validation loss
+    minposs = avg_valid_losses.index(min(avg_valid_losses))+1 
+    print('Lowest validation loss at epoch %d' %minposs)
+    
+    # visualize the loss and learning rate as the network trained
+    visualize_the_losses(avg_train_losses, avg_valid_losses)
+
+def train(train_loader, model, criterion, optimizer, use_cuda, epoch):
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    train_acc = AverageMeter()
+    
+    n_correct, n_total = 0, 0
+    log_interval = 35
+    
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    # pbar = tqdm(enumerate(train_loader))
+    for batch_idx, (data) in enumerate(train_loader):
+        inputs, targets = data # target size:(batch size,1), input size:(batch size, dim x win)
+        targets = targets.view(-1) # target size:(batch size)
+        current_sample = inputs.size(0) # batch size
+        
+        if use_cuda:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        output = model(inputs) # out size:(batch size, #classes=2)
+        
+        # calculate accuracy of predictions in the current batch
+        n_correct += (torch.max(output, 1)[1].long().view(targets.size()) == targets).sum().item()
+        n_total += current_sample
+        train_acc_temp = 100. * n_correct / n_total
+        train_acc.update(train_acc_temp, inputs.size(0))
+        
+        loss = criterion(output, targets)
+        losses.update(loss.item(), inputs.size(0))
+        
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if batch_idx % log_interval == 0:
+            print(
+                    'Train Epoch: {:3d} [{:8d}/{:8d} ({:3.0f}%)]\t'
+                    'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                    'Loss {loss.avg:.4f}\t'
+                    'Acc {train_acc.avg:.4f}'.format(
+                     epoch, batch_idx * len(inputs), len(train_loader.dataset),
+                     100. * batch_idx / len(train_loader), 
+                     batch_time=batch_time, loss=losses, train_acc=train_acc))
+    return losses.avg
+    
+def validate(val_loader, model, criterion, use_cuda, epoch):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    val_acc = AverageMeter()
+    
+    n_correct, n_total = 0, 0
+    
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (data) in enumerate(val_loader):
+            inputs, targets = data
+            targets = targets.view(-1) # target size:(batch size)
+            current_sample = inputs.size(0)  # batch size
+            
+            if use_cuda:
+                inputs = inputs.cuda()
+                targets = targets.cuda()
+            
+            # compute output
+            output = model(inputs)
+            
+            # measure accuracy and record loss
+            n_correct += (torch.max(output, 1)[1].long().view(targets.size()) == targets).sum().item()
+            n_total += current_sample
+            val_acc_temp = 100. * n_correct / n_total
+            val_acc.update(val_acc_temp, inputs.size(0))
+            
+            loss = criterion(output, targets)
+            losses.update(loss.item(), inputs.size(0))
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+        
+        print('  * Validation: '
+                  'Loss {loss.avg:.4f}\t'
+                  'Acc {val_acc.avg:.4f}'.format(
+                  loss=losses, val_acc=val_acc))
+    
+    return losses.avg
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
@@ -75,176 +246,41 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
-def create_optimizer(model, new_lr):
+def create_optimizer(optimizer, model, new_lr, wd):
     # setup optimizer
-    if args.optimizer == 'sgd':
+    if optimizer == 'sgd':
         optimizer = optim.SGD(model.parameters(), lr=new_lr,
                               momentum=0.9, dampening=0.9,
-                              weight_decay=args.wd)
-    elif args.optimizer == 'adam':
+                              weight_decay=wd)
+    elif optimizer == 'adam':
         optimizer = optim.Adam(model.parameters(), lr=new_lr,
-                               weight_decay=args.wd)
-    elif args.optimizer == 'adagrad':
+                               weight_decay=wd)
+    elif optimizer == 'adagrad':
         optimizer = optim.Adagrad(model.parameters(),
                                   lr=new_lr,
-                                  lr_decay=args.lr_decay,
-                                  weight_decay=args.wd)
+                                  weight_decay=wd)
     return optimizer
-
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.9 ** (epoch // 10))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-def main():
-    # Load training set and validation set
     
-    # Percentage of validation set
-    val_ratio = 10 
-
-    # Split training set into training set and validation set according to "val_ratio"
-    train_DB, valid_DB = split_train_dev(c.TRAIN_FEAT_DIR, val_ratio) 
+def visualize_the_losses(train_loss, valid_loss):
+    # https://github.com/Bjarten/early-stopping-pytorch/blob/master/MNIST_Early_Stopping_example.ipynb
+    # visualize the loss as the network trained
+    fig = plt.figure(figsize=(10,8))
+    plt.plot(range(1,len(train_loss)+1),train_loss, label='Training Loss')
+    plt.plot(range(1,len(valid_loss)+1),valid_loss, label='Validation Loss')
     
-    # Calculate global mean and standard deviation for normalization 
-    train_mean, train_std = get_global_mean_std(train_DB)
-        
-    if args.mfb:
-        transform = VAD_Compose([
-            #MyJointOp(),
-            truncatedinputfromMFB(), #(1, n_win=40, dim=40)
-            totensor_DNN_input()
-        ])
-        file_loader = read_MFB
-    else:
-        transform = transforms.Compose([
-            truncatedinput(),
-            toMFB(),
-            totensor(),
-            # tonormal()
-        ])
-        file_loader = read_audio
+    # find position of lowest validation loss
+    minposs = valid_loss.index(min(valid_loss))+1 
+    plt.axvline(minposs, linestyle='--', color='r',label='Early Stopping Checkpoint')
     
-    VAD_train_dataset = VAD_Dataset(DB=train_DB, loader=file_loader, transform=transform)
-    # print the experiment configuration
-    print('\nparsed options:\n{}\n'.format(vars(args)))
-    input_size = c.FILTER_BANK * c.NUM_FRAMES
-    print('\nInput dimension for training:\n{}\n'.format(input_size))
-    # instantiate model and initialize weights
-    model = DNN(input_size=input_size, hidden_size=args.hidden_size, num_classes=2)
-    #model = backgroundCNN(kernel_size=args.kernel_size, hidden_size=args.hidden_size,
-    #                      embedding_size=args.embedding_size, num_classes=n_classes)
-
-    #if torch.cuda.device_count() > 1:
-    #    print("Let's use", torch.cuda.device_count(), "GPUs!")
-    #    # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-    #    model = nn.DataParallel(model)
-
-    if args.cuda:
-        model.cuda()
-    start = args.start_epoch
-    end = start + args.epochs
-    # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().cuda(args.gpu_id)
-    optimizer = create_optimizer(model, args.lr)
-    # optionally resume from a checkpoint
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print('=> loading checkpoint {}'.format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            checkpoint = torch.load(args.resume)
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-        else:
-            print('=> no checkpoint found at {}'.format(args.resume))
-    VAD_train_loader = torch.utils.data.DataLoader(
-        dataset=VAD_train_dataset, batch_size=args.batch_size, shuffle=args.shuffle,
-        num_workers=args.workers)
-    for epoch in range(start, end):
-        adjust_learning_rate(optimizer, epoch)
-        # train for one epoch
-        train(VAD_train_loader, model, criterion, optimizer, epoch)
-        # do checkpointing
-        torch.save({'epoch': epoch + 1, 'state_dict': model.state_dict(),
-                    'optimizer': optimizer.state_dict()},
-                    '{}/checkpoint_{}.pth'.format(LOG_DIR, epoch))
-
-
-def train(train_loader, model, criterion, optimizer, epoch):
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    # train_acc = AverageMeter()
-    n_correct, n_total = 0, 0
-    # switch to train mode
-    model.train()
-
-    end = time.time()
-    # pbar = tqdm(enumerate(train_loader))
-    for batch_idx, (data) in enumerate(train_loader):
-        inputs, targets = data # target size:(batch size,1), input size:(batch size, dim x win)
-        #print(inputs.shape)
-        #print(targets.shape)
-        current_sample = inputs.size(0) # batch size
-        # delete the channel axis for DNN input
-        #inputs = inputs.resize_(1 * current_sample, inputs.size(2)) # size:(batch size, dim=1600)
-        # measure data loading time
-        data_time.update(time.time() - end)
-
-        inputs = Variable(inputs)
-        targets = Variable(targets)
-
-        if args.cuda:
-            inputs = inputs.cuda()
-            targets = targets.cuda()
-
-        out = model(inputs) # out size:(batch size, #classes=2)
-        #print(out.shape)
-        #_, out = model(inputs, targets)  # out size:(batch size, #classes), for lsoftmax
-
-        # calculate accuracy of predictions in the current batch
-        n_correct += (torch.max(out, 1)[1].long().view(targets.size()) == targets).sum().item()
-        n_total += current_sample
-        train_acc = 100. * n_correct/n_total
-        # train_acc.update(train_acc_temp, inputs.size(0))
-        #print(targets.size())
-        targets = targets.squeeze_()
-        #print(targets.size())
-        #cross entropy
-        #Input: (N,C) where C = number of classes
-        #Target: (N) where each value is 0=<targets[i]=<C-1
-        loss = criterion(out, targets)
-        losses.update(loss.item(), inputs.size(0))
-        # loss = temp_loss / current_sample # average the loss by minibatch
-        inf = float("inf")
-        loss_value = loss.data.item()
-        if loss_value == inf or loss_value == -inf:
-            print("WARNING: received an inf loss, setting loss value to 0")
-            loss_value = 0
-        # avg_loss += loss_value
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        #clip_grad_norm_(model.parameters(), max_norm=0.25)
-        optimizer.step()
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-        if batch_idx % args.log_interval == 0:
-            print(
-                'Train Epoch: {:3d} [{:8d}/{:8d} ({:3.0f}%)]\t'
-                'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                'Acc {train_acc:.4f}'.format(
-                    epoch, batch_idx * len(inputs), len(train_loader.dataset),
-                           100. * batch_idx / len(train_loader), batch_time=batch_time,
-                    data_time=data_time, loss=losses, train_acc=train_acc))
-        # len(train_loader.dataset) : # of utterances in all DB = 216753
-        # len(train_loader) : # of batches = 216753/batchsize(128) = 1694
-        # len(inputs) : batch size = 128
-
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.ylim(0, 3.5) # consistent scale
+    plt.xlim(0, len(train_loss)+1) # consistent scale
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    #plt.show()
+    fig.savefig('loss_plot.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
